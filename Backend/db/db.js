ba5efef3514a7d961ec4eb7f6b995fc8d3d4c39f
lovesharma2005 <122599088+lovesharma2005@users.noreply.github.com>
2025-06-30 14:54:31 +0530

    Change

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect("mongodb://localhost:27017")
+        await mongoose.connect("mongodb://localhost:27017/FinGuard")
         console.log('Db Connected')
     } catch (error) {
         console.log('DB Connection Error');
